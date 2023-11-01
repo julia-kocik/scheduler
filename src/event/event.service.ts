d4eb403d05523e00069a77f9ac8cc47f53d73f7b
julia-kocik <julia.kocik@gmail.com>
2023-11-01 15:47:58 +0100

    Add getEventById method

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { EventRepository } from './event.repository';
 import { EventEntity } from './event.entity';
 import { CreateEventDto } from './dto/create-event.dto';
@@ -12,11 +12,21 @@ export class EventService {
     }
 
     async createEvent(createEventDto: CreateEventDto): Promise<EventEntity> {
-        const { name, surname, email, date } = createEventDto
-        const newEvent: EventEntity = this.eventRepository.create({
-            name, surname, email, date
-        })
-        await this.eventRepository.save(newEvent)
-        return newEvent
+      const { name, surname, email, date } = createEventDto
+      const newEvent: EventEntity = this.eventRepository.create({
+          name, surname, email, date
+      })
+      await this.eventRepository.save(newEvent)
+      return newEvent
+    }
+
+    async getEventById(id: string): Promise<EventEntity> {
+      const specificEvent = await this.eventRepository.findOne({ where: {
+        id
+      }});
+      if(!specificEvent) {
+        throw new NotFoundException(`Event with id: ${id} not found`);
       }
+      return specificEvent
+    }
 }
