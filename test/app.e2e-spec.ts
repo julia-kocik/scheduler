fce1d922927bd436633423e3255db0c8ea44835c
julia-kocik <julia.kocik@gmail.com>
2023-11-05 16:36:33 +0100

    Add e2e test for get and post Event

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,24 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+
+
+const mockEvent: mockEventInterface = {
+  id: '1befbb09-782a-4924-9e51-98f8ec8069ee',
+  name: 'Julia',
+  surname: 'Test',
+  email: 'email@email.com',
+  date: new Date(1995, 11, 17)
+};
+
+interface mockEventInterface {
+  id: string
+  name: string
+  surname: string
+  email: string
+  date: Date
+}
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -20,5 +38,18 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello!');
+  });
+  it('(GET) - get all events', async () => {
+    return request(app.getHttpServer())
+      .get('/event')
+      .expect(200)
+  });
+
+  it('(POST) - create new event', async () => {
+    return request(app.getHttpServer())
+      .post('/event')
+      .send(mockEvent)
+      .expect(201)
+      .then((res) => expect(res.body.name).toBe('Julia'));
   });
 });
