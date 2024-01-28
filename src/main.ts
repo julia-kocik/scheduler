876bfc828768673bc769e8143d6d5b190f1f0036
julia-kocik <julia.kocik@gmail.com>
2024-01-28 20:58:33 +0100

    Revert "Add new packages"
    This reverts commit a2fd25bc14be167162be5ec20ed448672323cae4.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { createDocument } from 'swagger/swagger';
 import { SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,14 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('api/v1');
-  const expressApp = app.getHttpAdapter().getInstance();
-  expressApp.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    if (!req.secure) {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  });
-  expressApp.set('trust proxy', true);
   SwaggerModule.setup('api', app, createDocument(app));
   await app.listen(process.env.PORT || 80);
 }
